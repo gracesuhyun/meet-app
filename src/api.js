a393f3d343192cdaf38a80debd67a618e5625196
gracesuhyun <gracesuhyun@gmail.com>
2022-09-06 19:49:32 -0400

    Update api.js

--- src/api.js
+++ src/api.js
@@ -40,17 +40,10 @@ const getEvents = async () => {
     return mockData;
   }
 
-  // if (!navigator.onLine) {
-  //   const data = localStorage.getItem('lastEvents');
-  //   NProgress.done();
-  //   return data?JSON.parse(data).events:[];
-  // }
-
   if (!navigator.onLine) {
-    const { events } = await localStorage.getItem("lastEvents");
+    const data = localStorage.getItem('lastEvents');
     NProgress.done();
-    
-    return { events: JSON.parse(events), locations: extractLocations(events) };
+    return data?JSON.parse(data).events:[];
   }
 
   const token = await getAccessToken();
@@ -63,9 +56,9 @@ const getEvents = async () => {
       localStorage.setItem('lastEvents', JSON.stringify(result.data.events));
       localStorage.setItem('locations', JSON.stringify(locations));
     }
-    console.log(result.data.events);
+
     NProgress.done();
-    return result.data.events;
+    return { events: result.data.events, locations };
   }
 };
 
