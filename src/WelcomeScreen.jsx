edf2b885a437930afd5fdea33a2d70a9d8418013
gracesuhyun <gracesuhyun@gmail.com>
2022-09-05 20:17:08 -0400

    fix welcome screen button

--- src/WelcomeScreen.jsx
+++ src/WelcomeScreen.jsx
@@ -25,7 +25,7 @@ function WelcomeScreen(props) {
           </div>
         </div>
         <a
-        href="https://YOUR_GITHUB_USERNAME.github.io/meet/privacy.html"
+        href="https://gracesuhyun.github.io/meet/privacy.html"
         rel="nofollow noopener"
         >
         Privacy policy
