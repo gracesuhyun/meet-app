84a14d1df0c2529894eb9043c4b7ead44db0867d
gracesuhyun <gracesuhyun@gmail.com>
2022-09-12 17:01:29 -0400

    fix offline bug

--- src/api.js
+++ src/api.js
@@ -65,7 +65,7 @@ const getAccessToken = async () => {
   const accessToken = localStorage.getItem('access_token');
   const tokenCheck = accessToken && (await checkToken(accessToken));
 
-  if (!accessToken || tokenCheck.error) {
+  if (!accessToken || !tokenCheck) {
     await localStorage.removeItem('access_token');
     const searchParams = new URLSearchParams(window.location.search);
     const code = await searchParams.get('code');
