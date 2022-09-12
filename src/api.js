3e6508347067e6bc801d4c0906d9a0ed6eae4bac
gracesuhyun <gracesuhyun@gmail.com>
2022-09-12 14:29:05 -0400

    Update api.js

--- src/api.js
+++ src/api.js
@@ -22,7 +22,7 @@ const checkToken = async (accessToken) => {
   )
     .then((res) => res.json())
     .catch((error) => error.json());
-    return result;
+    return result.error ? false : true;
 };
 
 const extractLocations = (events) => {
