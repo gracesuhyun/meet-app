3641d934d2c137bd245192eb0d2900179539c3f3
gracesuhyun <gracesuhyun@gmail.com>
2022-09-13 01:14:27 -0400

    update scatter plot

--- src/App.js
+++ src/App.js
@@ -89,7 +89,7 @@ class App extends Component {
         <CitySearch 
           locations={locations} 
           updateEvents={this.updateEvents} />
-
+        <br/>
         <NumberOfEvents 
           // events={this.state.events}
           numberOfEvents={numberOfEvents}
@@ -108,7 +108,7 @@ class App extends Component {
         >
           <CartesianGrid />
           <XAxis type="category" dataKey="city" name="city" />
-          <YAxis type="number" dataKey="number" name="number of events" allowDecimals={false}/>
+          <YAxis type="number" dataKey="number" name="number of events" allowDecimals={false} width={800}/>
           <Tooltip cursor={{ strokeDasharray: '3 3' }} />
           <Scatter data={this.getData()} fill="#8884d8" />
         </ScatterChart>
